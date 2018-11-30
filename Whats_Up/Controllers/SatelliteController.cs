d53ddc726d91cd4aee577a6a22067b7580c7855d
Clayton <clayton.cox@gmail.com>
2018-11-30 14:08:27 -0500

    deleted unnecessary files

--- Whats_Up/Controllers/SatelliteController.cs
+++ Whats_Up/Controllers/SatelliteController.cs
@@ -43,5 +43,35 @@ namespace Whats_Up.Controllers
 
             return View();
         }
+        public ActionResult SatTracker()
+        {
+
+            string SatTracker = WebConfigurationManager.AppSettings["N2YO"];
+
+            UriBuilder builder = new UriBuilder
+            {
+                Scheme = "https",
+                Host = "n2yo.com",
+                Path = "/rest/v1/satellite/positions/25544//1/1/0/2/&apiKey=" + N2YO,
+            };
+
+            HttpWebRequest requestN2YO = WebRequest.CreateHttp(builder.ToString());
+            requestN2YO.UserAgent = "Mozilla / 5.0(Windows NT 6.1; WOW64; rv: 64.0) Gecko / 20100101 Firefox / 64.0";
+
+            HttpWebResponse reponse = (HttpWebResponse)requestN2YO.GetResponse();
+
+            if (reponse.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader reader = new StreamReader(reponse.GetResponseStream());
+
+                string output = reader.ReadToEnd();
+                JObject jSpaceObject = JObject.Parse(output);
+
+                ViewBag.ThisTest = jSpaceObject;
+                ViewBag.ThisTest2 = builder.ToString();
+            }
+
+            return View();
+        }
     }
 }
