af422a105cff888123b684e25fda685b6b87776f
Clayton <clayton.cox@gmail.com>
2018-11-30 11:04:18 -0500

    testing out new get ignore file

---
+++ Whats_Up/Models/Mapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Whats_Up.Models
+{
+    public class Mapper
+    {
+    }
+}
---
+++ Whats_Up/Models/SatelliteST.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Whats_Up.Models
+{
+    public class SatelliteST
+    {
+
+
+    }
+}
