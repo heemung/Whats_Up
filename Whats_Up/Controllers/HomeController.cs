f409d8a6d4bda5a0f924f627a0ca763dc5d44c58
Ty <Tmcarron7@gmail.com>
2018-12-03 14:51:48 -0500

    Added map

--- Whats_Up/Controllers/HomeController.cs
+++ Whats_Up/Controllers/HomeController.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Web.Configuration;
 
 namespace Whats_Up.Controllers
 {
     public class HomeController : Controller
     {
+        public string BingKey = WebConfigurationManager.AppSettings["Mapper"];
         public ActionResult Index()
         {
             return View();
@@ -18,6 +22,24 @@ namespace Whats_Up.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
+            
+            ViewBag.Key = BingKey;
+
+            return View();
+        }
+        public ActionResult InputLocation(string addressLine, string postalCode)
+        {
+            string URL = "http://dev.virtualearth.net/REST/v1/Locations/US/adminDistrict/postalCode/locality/addressLine?" +
+                "addressLine="+addressLine+"&postalCode="+postalCode+"&maxResults=5&key="+BingKey;
+            HttpWebRequest request = WebRequest.CreateHttp(URL);
+            request.UserAgent = "Mozilla / 5.0(Windows NT 6.1; Win64; x64; rv: 47.0) Gecko / 20100101 Firefox / 47.0";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string output = reader.ReadToEnd();
+                JObject JParser = JObject.Parse(output);
+            }
             return View();
         }
 
