8074330ef7cb3f759291d6bdfdb4c653675194db
Clayton <clayton.cox@gmail.com>
2018-12-03 10:46:02 -0500

    added sats cats to dic

--- Whats_Up/Controllers/SatelliteController.cs
+++ Whats_Up/Controllers/SatelliteController.cs
@@ -43,6 +43,73 @@ namespace Whats_Up.Controllers
 
             return View();
         }
+
+        //getting user selections for satellites categories 
+        public List<string> GetSatCat()
+        {
+            
+
+
+
+        }
+
+        public Dictionary<string,int> AddingCatsToList()
+        {
+            Dictionary<string,int> satCatDic = new Dictionary<string, int>();
+
+            satCatDic.Add("Yaogan", 36);
+            satCatDic.Add("XMandSirius", 33);
+            satCatDic.Add("WestfordNeedles", 37);
+            satCatDic.Add("Weather", 3);
+            satCatDic.Add("TV", 34);
+            satCatDic.Add("Tsiklon", 41);
+            satCatDic.Add("Tsikada", 42);
+            satCatDic.Add("Tselina" ,44);
+            satCatDic.Add("TrackingandDataRelaySatelliteSystem", 9);
+            satCatDic.Add("Strela", 39);
+            satCatDic.Add("Space&EarthScience", 26);
+            satCatDic.Add("Search&Rescue", 7);
+            satCatDic.Add("Satellite-BasedAugmentationSystem", 23);
+            satCatDic.Add("RussianLEONavigation", 25);
+            satCatDic.Add("Raduga", 13);
+            satCatDic.Add("RadarCalibration", 18);
+            satCatDic.Add("QZSS", 47);
+            satCatDic.Add("Parus", 38);
+            satCatDic.Add("Orbcomm", 16);
+            satCatDic.Add("O3BNetworks", 43);
+            satCatDic.Add("NOAA", 4);
+            satCatDic.Add("NavyNavigationSatelliteSystem", 24);
+            satCatDic.Add("Molniya", 14);
+            satCatDic.Add("Military", 30);
+            satCatDic.Add("Lemur", 49);
+            satCatDic.Add("ISS", 2);
+            satCatDic.Add("IRNSS", 46);
+            satCatDic.Add("Iridium", 15);
+            satCatDic.Add("Intelsat", 11);
+            satCatDic.Add("Gorizont", 12);
+            satCatDic.Add("Gonets", 40);
+            satCatDic.Add("GOES", 5);
+            satCatDic.Add("GlonassOperational", 21);
+            satCatDic.Add("Globalstar", 17);
+            satCatDic.Add("GlobalPositioningSystem(GPS)Operational", 20);
+            satCatDic.Add("GlobalPositioningSystem(GPS)Constellation", 50);
+            satCatDic.Add("Geostationary", 10);
+            satCatDic.Add("Geodetic", 27);
+            satCatDic.Add("Galileo", 22);
+            satCatDic.Add("Flock", 48);
+            satCatDic.Add("Experimental", 19);
+            satCatDic.Add("Engineering", 28);
+            satCatDic.Add("Education", 29);
+            satCatDic.Add("EarthResources", 6);
+            satCatDic.Add("DisasterMonitoring", 8);
+            satCatDic.Add("CubeSats", 32);
+            satCatDic.Add("Celestis", 45);
+            satCatDic.Add("Brightest", 1);
+            satCatDic.Add("BeidouNavigationSystem", 35);
+
+            return satCatDic;
+        }
+
         public ActionResult SatTracker()
         {
 
