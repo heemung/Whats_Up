0462db43b5ee0e3afc14cf6cc7e1e7ca0eaea307
Ty <Tmcarron7@gmail.com>
2018-12-07 15:09:18 -0500

    Hopefully migrations work after this

---
+++ Whats_Up/Migrations/201812071851469_MoreShit.cs
@@ -0,0 +1,16 @@
+namespace Whats_Up.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MoreShit : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
